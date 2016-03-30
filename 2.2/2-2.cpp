07b9e920a7dd614d05979003a8286ad4d951063c
roman <roman@debian>
2016-03-30 18:01:25 -0400

    Added sample program 2.2

---
+++ 2.2/2-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int fleas;	//vytvori celociselnou promennou
+
+	fleas = 38;	//priradi promenne hodnotu
+	cout << "Moje kocka ma ";
+	cout << fleas;	//zobrazi hodnotu promenne fleas
+	cout << " blech.\n";
+	return 0;
+}
